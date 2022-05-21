0b528639cd8d8b935300742712279aa7dd710973
Wes Bryie <wesleybryie@protonmail.com>
2022-05-21 17:32:06 +0000

    hi

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Read My IP!</h1>
+
+      <button onClick={console.log("hi")}>hi ho here we go</button>
+
+    </div>
+  );
+}
+
+export default App;
