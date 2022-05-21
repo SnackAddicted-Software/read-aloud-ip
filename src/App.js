bef6bedc40551bfec870b47cc43d6f513c34dcab
Wes Bryie <wesleybryie@protonmail.com>
2022-05-21 17:53:25 +0000

    And thanks to medium posters again, it is "done"

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
 import './App.css';
+import {useState,useEffect} from 'react'
+import { useSpeechSynthesis } from "react-speech-kit";
+import axios from 'axios'
 
 function App() {
+  //creating IP state
+  const [ip, setIP] = useState('');
+  const { speak } = useSpeechSynthesis();
+
+  //creating function to load ip address from the API
+  const getData = async () => {
+    const res = await axios.get('https://geolocation-db.com/json/')
+    console.log(res.data);
+    setIP(res.data.IPv4)
+  }
+
+  useEffect( () => {
+    //passing getData method to the lifecycle method
+    getData()
+
+  }, [])
+
   return (
     <div className="App">
       <h1>Read My IP!</h1>
 
-      <button onClick={console.log("hi")}>hi ho here we go</button>
-
+      <button onClick={() => speak({ text: ip })}>hi ho here we go</button>
+      <h2>Your IP Address is</h2>
+      <h4>{ip}</h4>
     </div>
   );
 }
+
 
 export default App;
